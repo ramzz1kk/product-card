cf7f603ba61528e1e9b9cfb6d04711af12cf9338
Ramzat <ramzat1982@gmail.com>
2026-07-25 06:23:45 +0600

    Fixed errors

--- homework-10.js
+++ homework-10.js
@@ -1,4 +1,4 @@
-import {productCards} from "./product-cards.js";
+import { productCards } from "./product-cards.js";
 
 const productCardTemplate = document.getElementById("product-card-template");
 const productsList = document.querySelector(".products");
@@ -6,7 +6,7 @@ const productsList = document.querySelector(".products");
 function getCardCount() {
   const count = Number(prompt("Сколько карточек отобразить? От 1 до 5"));
   if (count >= 1 && count <= 5) {
-    return count
+    return count;
   }
   alert("Введите число от 1 до 5");
   return 0;
@@ -16,17 +16,19 @@ function renderCards(cards) {
   cards.forEach((card) => {
     const cardClone = productCardTemplate.content.cloneNode(true);
 
-    cardClone.querySelector(".card__image").src =`img/${card.cardImage}`;
-    cardClone.querySelector(".card__image").alt = card.imageAlt;
+    cardClone.querySelector(".card__image").src = `img/${card.cardImage}.png`;
     cardClone.querySelector(".card__category").textContent = card.cardCategory;
     cardClone.querySelector(".card__name").textContent = card.cardName;
     cardClone.querySelector(".card__description p").textContent = card.cardDescription;
-    const compoundItems = cardClone.querySelectorAll(".compound__list li");
 
-    compoundItems.forEach((item, index) => {
-      item.textContent = card.compoundList[index];
+    const compoundList = cardClone.querySelector(".compound__list");
+
+    card.compoundList.forEach((compound) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = compound;
+      compoundList.append(listItem);
     });
-    
+
     cardClone.querySelector(".card__price span").textContent = `${card.price} ₽`;
     productsList.append(cardClone);
   });
@@ -35,11 +37,14 @@ const count = getCardCount();
 
 const cards = productCards.slice(0, count);
 
-const productsDescription = productCards.reduce((acc, product) => [
-  ...acc,
-  {
-    [product.cardName]: product.cardDescription
-  }
-], []);
+const productsDescription = productCards.reduce(
+  (acc, product) => [
+    ...acc,
+    {
+      [product.cardName]: product.cardDescription,
+    },
+  ],
+  [],
+);
 
 renderCards(cards);
--- product-cards.js
+++ product-cards.js
@@ -1,7 +1,6 @@
 const productCards = [
 {
-  cardImage: "moisturizing-mousse.png",
-  cardImageAlt: "Увлажняющий мусс",
+  cardImage: "moisturizing-mousse",
   cardCategory:"для нормальной кожи",
   cardName: "Увлажняющий мусс",
   cardDescription:"Глубоко увлажняет кожу лица, оставляя её мягкой и гладкой.",
@@ -13,8 +12,7 @@ const productCards = [
   price: 2750
 },
 {
-  cardImage: "moisturizing-mask.png",
-  cardImageAlt: "Увлажняющая маска",
+  cardImage: "moisturizing-mask",
   cardCategory:"для нормальной кожи",
   cardName: "Увлажняющая маска",
   cardDescription:"Способствует удерживанию влаги в верхних слоях кожи.",
@@ -26,8 +24,7 @@ const productCards = [
   price: 3500
 },
 {
-  cardImage: "cleansing-gel.png",
-  cardImageAlt: "Гель для умывания",
+  cardImage: "cleansing-gel",
   cardCategory:"для нормальной кожи",
   cardName:"Гель для умывания",
   cardDescription:"Интенсивно очищает, не повреждает защитный барьер кожи.",
@@ -39,8 +36,7 @@ const productCards = [
   price: 2750
 },
 {
-  cardImage: "gift-set1.png",
-  cardImageAlt: "Подарочный набор №1",
+  cardImage: "gift-set1",
   cardCategory:"для нормальной кожи",
   cardName:"Подарочный набор №1",
   cardDescription:"Набор, состоящий из увлажняющего крема и маски.",
@@ -52,8 +48,7 @@ const productCards = [
   price: 2750
 },
 {
-  cardImage: "gift-set5.png",
-  cardImageAlt: "Подарочный набор №5",
+  cardImage: "gift-set5",
   cardCategory:"для нормальной кожи",
   cardName:"Подарочный набор №5",
   cardDescription:"Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.",
