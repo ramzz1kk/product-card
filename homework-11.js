9bc7d1c6de9c8b1e189b865be4f25749c5f1168b
Ramzat <ramzat1982@gmail.com>
2026-08-01 16:20:26 +0600

    исправил ошибки и подправил табуляцию

--- homework-11.js
+++ homework-11.js
@@ -1,26 +1,30 @@
-const emailForm = document.querySelector('#email-form')
-emailForm.addEventListener('submit',(event)=>{
-  event.preventDefault ();
-  const form = event.target;
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData.entries());
-  console.log(data)
-})
+const emailForm = document.querySelector("#email-form");
 
-const modal = document.querySelector (".modal");
-const registrationFormBtn = document.querySelector (".registration-form-btn")
-const closeModalBtn =document.querySelector(".close-modal-btn")
+function getFormData(form) {
+  const formData = new FormData(form);
+  return Object.fromEntries(formData.entries());
+}
+emailForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const data = getFormData(form);
+  console.log(data);
+});
+
+const modal = document.querySelector(".modal");
+const registrationFormBtn = document.querySelector(".registration-form-btn");
+const closeModalBtn = document.querySelector(".close-modal-btn");
 
 function openModal() {
-modal.style.display = "flex"
+  modal.style.display = "flex";
 }
 
 function closeModal() {
-  modal.style.display = "none"
+  modal.style.display = "none";
 }
 
-closeModalBtn.onclick = closeModal
-registrationFormBtn.onclick = openModal
+closeModalBtn.onclick = closeModal;
+registrationFormBtn.onclick = openModal;
 
 window.onclick = function (event) {
   if (event.target === modal) {
@@ -29,19 +33,18 @@ window.onclick = function (event) {
 };
 
 let user;
-const regForm = document.querySelector('.registration-form')
-regForm.addEventListener('submit', (event) => {
-  event.preventDefault ();
-  const password = document.querySelector ("#user-password").value
-  const confirmPassword = document.querySelector ("#confirm-password").value
+const regForm = document.querySelector(".registration-form");
+regForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const password = document.querySelector("#user-password").value;
+  const confirmPassword = document.querySelector("#confirm-password").value;
   if (!regForm.checkValidity() || password !== confirmPassword) {
     alert("Форма заполнена не коректно");
     return;
   }
-  const regFormData = new FormData(regForm);
-  user = Object.fromEntries(regFormData.entries())
+  user = getFormData(regForm);
   user.createdOn = new Date();
-  console.log (user);
+  console.log(user);
   modal.style.display = "none";
-  alert()
+  alert("Пользователь успешно зарегистрирован!");
 });
